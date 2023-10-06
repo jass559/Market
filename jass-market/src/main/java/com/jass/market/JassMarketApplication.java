8e333523b4271aff0e6e8f539ee041dd37f38773
john <jass559@gmail.com>
2023-10-05 22:57:26 -0400

    creada la estructura del proyecto

---
+++ jass-market/src/main/java/com/jass/market/JassMarketApplication.java
@@ -0,0 +1,15 @@
+package com.jass.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JassMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JassMarketApplication.class, args);
+
+		System.out.println("ok");
+	}
+
+}
---
+++ jass-market/src/test/java/com/jass/market/JassMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jass.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JassMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
